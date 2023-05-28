2cc3708871ef50718d343f97cfa3869adda868db
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2023-05-28 17:30:45 +0330

    initial client

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import CryptoList from "./Pages/CryptoList";
+
+const App = () => {
+  return (
+    <section className="min-h-screen">
+      <section className="gradient-bg-welcome">
+        {/* Navbar component */}
+        {/* Intro component */}
+      </section>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cryptocurrencies" element={<CryptoList />} />
+      </Routes>
+
+    </section>
+  )
+}
+
+export default App;
---
+++ client/src/Pages/CryptoList.jsx
@@ -0,0 +1,9 @@
+const CryptoList = () => {
+    return (
+        <>
+            <h1>Cryptocurrencies List</h1>
+        </>
+    )
+}
+
+export default CryptoList;
